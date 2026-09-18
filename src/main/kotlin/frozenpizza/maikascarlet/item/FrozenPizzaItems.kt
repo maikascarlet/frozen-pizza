a8d0f48397558c92f0e29cf445fe7a9b862f7d3c
maikascarlet <maikascarlet@users.noreply.github.com>
2026-09-17 20:28:34 -0400

    Add tomato sauce item and 1.5.0 release

--- src/main/kotlin/frozenpizza/maikascarlet/item/FrozenPizzaItems.kt
+++ src/main/kotlin/frozenpizza/maikascarlet/item/FrozenPizzaItems.kt
@@ -13,6 +13,8 @@ import net.minecraft.world.item.Item
 object FrozenPizzaItems {
     private val FROZEN_PIZZA_KEY: ResourceKey<Item> =
         ResourceKey.create(Registries.ITEM, FrozenPizza.id("frozen_pizza"))
+    private val TOMATO_SAUCE_KEY: ResourceKey<Item> =
+        ResourceKey.create(Registries.ITEM, FrozenPizza.id("tomato_sauce"))
 
     val FROZEN_PIZZA: Item = Item(
         Item.Properties()
@@ -26,10 +28,14 @@ object FrozenPizzaItems {
             )
     )
 
+    val TOMATO_SAUCE: Item = Item(Item.Properties().setId(TOMATO_SAUCE_KEY))
+
     fun register() {
         Registry.register(BuiltInRegistries.ITEM, FROZEN_PIZZA_KEY, FROZEN_PIZZA)
+        Registry.register(BuiltInRegistries.ITEM, TOMATO_SAUCE_KEY, TOMATO_SAUCE)
         ItemGroupEvents.modifyEntriesEvent(CreativeModeTabs.FOOD_AND_DRINKS).register { entries ->
             entries.accept(FROZEN_PIZZA)
+            entries.accept(TOMATO_SAUCE)
         }
     }
 }
